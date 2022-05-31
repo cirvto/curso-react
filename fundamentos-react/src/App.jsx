0d1382a11841c9c15ea459dfdedf7a81aada5c78
Victor <thomasvictor462@gmail.com>
2022-05-31 17:31:34 -0300

    Aula 13, 14 e 15 - Adicionando estilos únicos com props.children

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import React from 'react'
 
-import Card from './components/layout/card'
+import Card from './components/layout/Card'
 import Aleatorio from './components/basicos/Aleatorio'
 import Fragmento from './components/basicos/Fragmento'
 import ComParametro from './components/basicos/ComParametro'
@@ -11,20 +11,20 @@ export default _ =>
     <div className='App'>
         <h1>Fundamentos React</h1>
         <div className='Cards'>
-            <Card titulo='Desafio Aleatório'>
+            <Card titulo='Desafio Aleatório' color='#FFC233'>
                 <Aleatorio min={1} max={60}/>
             </Card>
-            <Card titulo='Fragmento'>
+            <Card titulo='Fragmento' color='#EFBA3D'>
                 <Fragmento />
             </Card>
-            <Card titulo='Com Parâmetro'>
+            <Card titulo='Com Parâmetro' color='#DFB146'>
             <ComParametro
                 titulo='Produtos TECH por menos de R$50,00'
                 p1='Light Bar Monitor'
                 p2='Mouse Sem Fio'
                 p3='Mousepad Minimalista' />
             </Card>
-            <Card titulo='Primeiro Componente'>
+            <Card titulo='Primeiro Componente' color='#CFA950'>
                 <Primeiro />
             </Card>
         </div>
