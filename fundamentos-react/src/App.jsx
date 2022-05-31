0bb9e8391455a0e18e825aa63dd36ef7c4c4e2c4
Victor <thomasvictor462@gmail.com>
2022-05-31 12:02:00 -0300

    Criando componentes Card e introduzindo os componentes criados anteriormente dentro dos cards

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import Card from './components/layout/card'
 import Aleatorio from './components/basicos/Aleatorio'
 import Fragmento from './components/basicos/Fragmento'
 import ComParametro from './components/basicos/ComParametro'
@@ -8,12 +9,20 @@ import Primeiro from './components/basicos/Primeiro'
 export default _ =>
     <div id='app'>
         <h1>Fundamentos React</h1>
-        <Aleatorio min={1} max={60}/>
-        <Fragmento />
+        <Card titulo='Desafio Aleatório'>
+            <Aleatorio min={1} max={60}/>
+        </Card>
+        <Card titulo='Fragmento'>
+            <Fragmento />
+        </Card>
+        <Card titulo='Com Parâmetro'>
         <ComParametro
             titulo='Produtos TECH por menos de R$50,00'
             p1='Light Bar Monitor'
             p2='Mouse Sem Fio'
             p3='Mousepad Minimalista' />
-        <Primeiro />
+        </Card>
+        <Card titulo='Primeiro Componente'>
+            <Primeiro />
+        </Card>
     </div>
