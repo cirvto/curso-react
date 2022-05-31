8ce40c4c693c1d16d3d1762a9e7975af6c699b9a
Victor <thomasvictor462@gmail.com>
2022-05-31 08:12:24 -0300

    Aula 08 - Props são somente leitura

--- fundamentos-react/src/components/basicos/ComParametro.jsx
+++ fundamentos-react/src/components/basicos/ComParametro.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function ComParametro(props) {
     const displayAnother = props.p3.length > 0 ? props.p3 : 'Não existe.'
+    // Props são somente LEITURA, ou seja, o código abaixo irá gerar um erro.
+    // props.p1 = `${props.p1} é muito bom`
+    // O certo é criar um novo espaço de armazenamento e inserir a mudançã que você deseja.
+    const p4 = `${props.p1} é muito bom`
     return (
         <div>
             <h2>{props.titulo}</h2>
@@ -10,6 +14,7 @@ export default function ComParametro(props) {
                 <li>{props.p2}</li>
                 <li>{props.p3}</li>
                 <li>{displayAnother}</li>
+                <li>{p4}</li>
             </ol>
         </div>
     )
