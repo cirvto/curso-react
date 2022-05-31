028a1d4a1ef8fd07686402f12c4787cf7903bc57
Victor <thomasvictor462@gmail.com>
2022-05-31 13:48:09 -0300

    Aula 13, 14 e 15 - Adicionando estilo nos cards e trabalhando com Flexbox para ajustar o tamanho e a responsividade da página"

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,3 +1,4 @@
+import './App.css'
 import React from 'react'
 
 import Card from './components/layout/card'
@@ -7,22 +8,24 @@ import ComParametro from './components/basicos/ComParametro'
 import Primeiro from './components/basicos/Primeiro'
 
 export default _ =>
-    <div id='app'>
+    <div className='App'>
         <h1>Fundamentos React</h1>
-        <Card titulo='Desafio Aleatório'>
-            <Aleatorio min={1} max={60}/>
-        </Card>
-        <Card titulo='Fragmento'>
-            <Fragmento />
-        </Card>
-        <Card titulo='Com Parâmetro'>
-        <ComParametro
-            titulo='Produtos TECH por menos de R$50,00'
-            p1='Light Bar Monitor'
-            p2='Mouse Sem Fio'
-            p3='Mousepad Minimalista' />
-        </Card>
-        <Card titulo='Primeiro Componente'>
-            <Primeiro />
-        </Card>
+        <div className='Cards'>
+            <Card titulo='Desafio Aleatório'>
+                <Aleatorio min={1} max={60}/>
+            </Card>
+            <Card titulo='Fragmento'>
+                <Fragmento />
+            </Card>
+            <Card titulo='Com Parâmetro'>
+            <ComParametro
+                titulo='Produtos TECH por menos de R$50,00'
+                p1='Light Bar Monitor'
+                p2='Mouse Sem Fio'
+                p3='Mousepad Minimalista' />
+            </Card>
+            <Card titulo='Primeiro Componente'>
+                <Primeiro />
+            </Card>
+        </div>
     </div>
