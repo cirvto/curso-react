038386af6f5904ac5060a3acb54132d0982a39d2
Victor <thomasvictor462@gmail.com>
2022-06-01 09:37:00 -0300

    Aula 16 e 17 - Passando propriedades de componente pai para componentes filhos

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,7 +1,9 @@
 import './App.css'
 import React from 'react'
-
 import Card from './components/layout/Card'
+
+import Familia from './components/basicos/Familia'
+import FamiliaMembro from './components/basicos/FamiliaMembro'
 import Aleatorio from './components/basicos/Aleatorio'
 import Fragmento from './components/basicos/Fragmento'
 import ComParametro from './components/basicos/ComParametro'
@@ -11,6 +13,13 @@ export default _ =>
     <div className='App'>
         <h1>Fundamentos React</h1>
         <div className='Cards'>
+            <Card titulo='Componente com Filhos' color='#FFC100'>
+                <Familia sobrenome='Silva'>
+                    <FamiliaMembro nome='João'/>
+                    <FamiliaMembro nome='José'/>
+                    <FamiliaMembro nome='Maria'/>
+                </Familia>
+            </Card>
             <Card titulo='Desafio Aleatório' color='#FFC233'>
                 <Aleatorio min={1} max={60}/>
             </Card>
