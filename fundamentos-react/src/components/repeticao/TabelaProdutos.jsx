a1c83e42815807cc309c56a396169fb5aef10714
Victor <thomasvictor462@gmail.com>
2022-06-02 11:13:07 -0300

    Aula de Correção - Melhorias no estilo do desafio, implementando uma tabela zebrada.

--- fundamentos-react/src/components/repeticao/TabelaProdutos.jsx
+++ fundamentos-react/src/components/repeticao/TabelaProdutos.jsx
@@ -1,22 +1,33 @@
 import React from 'react'
 import produtos from '../../data/produtos'
+import './TabelaProdutos.css'
 
 export default props => {
-    const produtosJSX = produtos.map((produto) => {
+    const produtosJSX = produtos.map((produto, i) => {
         const preco = produto.preco.toLocaleString('pt-BR', {
             style: 'currency',
             currency: 'BRL',
         }) 
         return (
-            <tr key={produto.id}>
-                <td style={{border: '1px solid red'}}>{produto.nome}</td>
-                <td style={{border: '1px solid red'}}>{preco}</td>
-            </tr>
+            <tbody>
+                <tr key={produto.id} className={i % 2 === 0 ? 'Par' : 'Impar'}>
+                    <td>{produto.id}</td>
+                    <td>{produto.nome}</td>
+                    <td>{preco}</td>
+                </tr>
+            </tbody>
         )
     })
     return (
         <div>
-            <table style={{border: '1px solid red'}}>
+            <table className='TabelaProdutos'>
+                <thead>
+                    <tr>
+                        <td>Posição</td>
+                        <td>Nome</td>
+                        <td>Preço</td>
+                    </tr>
+                </thead>
                 {produtosJSX}
             </table>
         </div>
